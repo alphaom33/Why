d5d8a6b52d26e0b8d12e37aa5d4d20823ec3d4fc
alphaom33 <alphaom34@gmail.com>
2024-07-20 14:43:37 -0500

    I don't remember what these are

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,15 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+	title: {
+		template: '%s | Acme Dashboard',
+		default: 'Acme Dashboard',
+	},
+	description: 'Yay Next',
+	metadataBase: new URL('https://no'),
+};
 
 export default function RootLayout({
   children,
@@ -7,7 +17,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+   <html lang="en">
       <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
